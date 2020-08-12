5bd9f390923babb2b42132bc12650aa84e2e94dc
Sonu <sonu3660@gmail.com>
2020-08-12 15:07:05 +0530

    Add active class into Accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
+   // Initialization of state by Array Destructuring.
+   const [activeIndex, setActiveIndex] = useState(null);
+
    // Helper function
    const onTitleClick = (index) => {
-      console.log(index);
+      //   console.log(index);
+      setActiveIndex(index);
    };
 
    // index is the second argument of map() method
    const renderedItems = items.map((item, index) => {
+      // title and content will active when we click on it.
+      const active = index === activeIndex ? "active" : "";
       return (
          <React.Fragment key={item.title}>
-            <div className="title active" onClick={() => onTitleClick(index)}>
+            <div
+               className={`title ${active}`}
+               onClick={() => onTitleClick(index)}
+            >
                <i className="dropdown icon"></i>
                {item.title}
             </div>
-            <div className="content active">
+            <div className={`content ${active}`}>
                <p>{item.content}</p>
             </div>
          </React.Fragment>
