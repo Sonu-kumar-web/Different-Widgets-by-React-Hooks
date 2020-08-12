afc514b12bc8d050495cda174f6129e55c1c5f1e
Sonu <sonu3660@gmail.com>
2020-08-13 00:12:56 +0530

    executing the request from useEfffect() and create a state for results

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,17 +2,33 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
-   const [term, setTerm] = useState("");
+   const [term, setTerm] = useState("Programming");
+   const [results, setResults] = useState([]);
+
+   console.log(results);
 
    useEffect(() => {
       const search = async () => {
-         await axios.get("abc");
+         const { data } = await axios.get(
+            "https://en.wikipedia.org/w/api.php",
+            {
+               params: {
+                  action: "query",
+                  list: "search",
+                  origin: "*",
+                  format: "json",
+                  srsearch: term,
+               },
+            }
+         );
+         setResults(data.query.search);
       };
+
       search();
    }, [term]);
 
    const onTextChange = (event) => {
-      console.log(event.target.value);
+      //   console.log(event.target.value);
       setTerm(event.target.value);
    };
 
