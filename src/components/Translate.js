4f49e8e442a1664cb5f6f9a44f8b67aa25a7055e
Sonu <sonu3660@gmail.com>
2020-08-14 16:47:28 +0530

    Add Convert component

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,7 @@
+// API Key=> "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM"
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
+import Convert from "./Convert";
 
 const options = [
    {
@@ -14,18 +16,33 @@ const options = [
       label: "Hindi",
       value: "Hi",
    },
+   {
+      label: "Bengali",
+      value: "bn",
+   },
 ];
 
 const Translate = () => {
    const [language, setLanguage] = useState(options[0]);
+   const [text, setText] = useState("");
 
    return (
       <div>
+         <div className="ui form">
+            <div className="field">
+               <label>Enter text</label>
+               <input value={text} onChange={(e) => setText(e.target.value)} />
+            </div>
+         </div>
          <Dropdown
+            label="Select a Language"
             options={options}
             selected={language}
             onSelectedChange={setLanguage}
          />
+         <hr />
+         <h3 className="ui header">Output</h3>
+         <Convert text={text} language={language} />
       </div>
    );
 };
