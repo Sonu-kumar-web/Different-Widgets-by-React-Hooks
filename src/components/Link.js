31c5c2605088cb351c09158aae1f9dfc6092d499
Sonu <sonu3660@gmail.com>
2020-08-15 22:33:07 +0530

    Update Route

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,7 +2,16 @@ import React from "react";
 
 const Link = ({ className, href, children }) => {
    const onClick = (event) => {
+      //this is used for 1) In Window "CTRL+click", In Mack "CMD+click" then link will be open in new Tab
+      if (event.metaKey || event.ctrlKey) {
+         return;
+      }
+
       event.preventDefault();
+      window.history.pushState({}, "", href);
+
+      const navEvent = new PopStateEvent("popstate");
+      window.dispatchEvent(navEvent);
    };
 
    return (
